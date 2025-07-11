5da5f953b76051beb790e4534e63bdcd21637c1e
Yunin Tias Triayusi Purbaningrum <yunintias@MacBook-Air-Yunin.local>
2025-07-11 20:44:02 +0700

    maybe let use custom transversal not networkx

--- src/orkes/core.py
+++ src/orkes/core.py
@@ -1,6 +1,7 @@
 from agents.core import AgentInterface
 from typing import Callable
 from orkes.utils import function_assertion
+import networkx as nx
 
 # Sentinel constants
 START = "__start__"
@@ -11,20 +12,40 @@ class Orkes:
     def __init__(self, state):
         self.node_pools = {}
         self.state = state
+        self.graphs = nx.DiGraph()
+        self._freeze = False
 
 
     def add_node(self, name: str, node: Callable):
+        if self._freeze:
+            raise RuntimeError("Cannot modify after compile")
         if name in self.node_pools:
             raise ValueError(f"Agent '{name}' already exists.")
 
         assert function_assertion(node, type(self.state)), (
             f"No parameter of 'node' has type matching self.state ({type(self.state)})."
         )
-        self.node_pools[name] = node
+        self.graphs.add_node(name, func=node)
 
     def add_edge(self, from_node, to_node):
+        if self._freeze:
+            raise RuntimeError("Cannot modify after compile")
+        self.graphs.add_edge(from_node, to_node)
+
+    def add_conditional_edges(self, from_node, judge_node, outcome_to_node):
         pass
-    
+
+    def run(self, query):
+        for node in nx.topological_sort(self.graphs):
+            func = self.graphs.nodes[node].get("func")
+            if func:
+                func(query)
+            
+
+        pass
+
+    def compile(self):
+        pass
 
 # function example
 # "function with state argument + agent is the agreed node"
