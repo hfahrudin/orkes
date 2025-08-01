8cf834947d74fe4d0a6d133eeef0cf55a9bb32a0
Hasby Fahrudin <fahrudinhasby12@gmail.com>
2025-08-01 17:58:15 +0900

    circular import

---
+++ test/graph_test.py
@@ -0,0 +1,41 @@
+
+import sys
+import os
+
+# Get the absolute path of folder A and add it to sys.path
+sys.path.insert(0, os.path.abspath('../')) 
+
+from orkes.graph.core import OrkesGraph
+from orkes.graph.unit import Node
+from typing import TypedDict
+
+class State(TypedDict):
+    topic: str
+    joke: str
+    improved_joke: str
+    final_joke: str
+
+
+def multiply_by_2(x, state):
+    result = x * 2
+    state['multiply_result'] = result  # save intermediate result
+    return result
+
+def add_3(x, state):
+    result = x + 3
+    state['add_result'] = result
+    return result
+
+def greater_than_10(x, state):
+    result = x > 10
+    state['condition_result'] = result
+    return result
+
+# Initialize graph state as an empty dict
+agent_graph = OrkesGraph(State)
+
+agent_graph.add_node('multiply_by_2' , multiply_by_2)
+agent_graph.add_node('add_3' , add_3)
+agent_graph.add_node('greater_than_10' , greater_than_10)
+
+
