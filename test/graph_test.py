2e7c658d5458ab9c7e4b44b239aa3b9f6c2e2573
Hasby Fahrudin <fahrudinhasby12@gmail.com>
2025-08-04 12:01:52 +0900

    fix circular and assetion error

--- test/graph_test.py
+++ test/graph_test.py
@@ -16,26 +16,24 @@ class State(TypedDict):
     final_joke: str
 
 
-def multiply_by_2(x, state):
-    result = x * 2
+def multiply_by_2(state: State):
+    result = 1 * 2
     state['multiply_result'] = result  # save intermediate result
     return result
 
-def add_3(x, state):
-    result = x + 3
+def add_3(state: State):
+    result = 2 + 3
     state['add_result'] = result
     return result
 
-def greater_than_10(x, state):
-    result = x > 10
+def greater_than_10(state: State):
+    result = 3 > 10
     state['condition_result'] = result
     return result
 
 # Initialize graph state as an empty dict
 agent_graph = OrkesGraph(State)
-
 agent_graph.add_node('multiply_by_2' , multiply_by_2)
 agent_graph.add_node('add_3' , add_3)
 agent_graph.add_node('greater_than_10' , greater_than_10)
-
 
