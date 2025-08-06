217412c4b15184a98e90685dec9a600ff486fec7
Hasby Fahrudin <fahrudinhasby12@gmail.com>
2025-08-06 14:07:21 +0900

    progress on graph integrity check

--- test/graph_test.py
+++ test/graph_test.py
@@ -33,7 +33,21 @@ def greater_than_10(state: State):
 
 # Initialize graph state as an empty dict
 agent_graph = OrkesGraph(State)
+END_node = agent_graph.END
+START_node = agent_graph.START
 agent_graph.add_node('multiply_by_2' , multiply_by_2)
 agent_graph.add_node('add_3' , add_3)
 agent_graph.add_node('greater_than_10' , greater_than_10)
 
+agent_graph.add_edge("add_3", "multiply_by_2")
+agent_graph.add_edge("multiply_by_2", "greater_than_10")
+
+agent_graph.add_edge(START_node, "add_3")
+agent_graph.add_edge("greater_than_10", END_node)
+# agent_graph.set_entry_point("multiply_by_2")
+# agent_graph.set_end_point("greater_than_10")
+
+agent_graph.compile()
+
+
+print(agent_graph._nodes_pool)
